83ed1f5ddf8379e52f7e590c88f3b681e523b009
kaietanpaul <kaietanpaul@gmail.com>
2024-08-31 23:08:40 +0200

    init.c norminette

--- src/init.c
+++ src/init.c
@@ -1,12 +1,13 @@
 #include "../include/fractol.h"
-t_mlx *init_mlx_win_img(t_mlx *data, int x, int y, char *title)
+
+t_mlx	*init_mlx_win_img(t_mlx *data, int x, int y, char *title)
 {
 	data = malloc(sizeof(t_mlx));
 	data->width = x;
 	data->height = y;
 	data->name = title;
-	data->minmax = (t_minmax){-2.0, 2.0, -2.0, 2.0};  // Initialize minmax for Mandelbrot
-	data->comp = (t_complex){0.285, 0.01};     // Default Julia constant
+	data->minmax = (t_minmax){-2.0, 2.0, -2.0, 2.0};
+	data->comp = (t_complex){0.285, 0.01};
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, x, y, title);
 	data->img.img = mlx_new_image(data->mlx, data->width, data->height);
