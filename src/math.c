0b37ac16d7f03a09e456f18d7b7a7d79be24a8e7
kaietanpaul <kaietanpaul@gmail.com>
2024-08-31 23:05:23 +0200

    norminette for math restored

--- src/math.c
+++ src/math.c
@@ -1,16 +1,16 @@
 #include "../include/fractol.h"
 
-double	scale(double num, double old_min, double old_max, double new_min, double new_max)
+double	scale(double num, double old_max, double new_min, double new_max)
 {
-	return ((new_max - new_min) * (num - old_min) / (old_max - old_min) + new_min);
+	return ((new_max - new_min) * (num - 0) / (old_max - 0) + new_min);
 }
+
 double	r_comp(int x, int width, double real_min, double real_max)
 {
-	return scale(x, 0, width, real_min, real_max);
-
+	return (scale(x, 0, width, real_min, real_max));
 }
 
 double	i_comp(int y, int height, double imag_min, double imag_max)
 {
-	return scale(y, 0, height, imag_min, imag_max);
+	return (scale(y, 0, height, imag_min, imag_max));
 }
