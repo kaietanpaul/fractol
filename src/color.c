9f96b11be8b2b0d75dc0d47314d35d8fb0c63c87
kaietanpaul <kaietanpaul@gmail.com>
2024-09-01 14:16:41 +0200

    color.c re-done + norminette

--- src/color.c
+++ src/color.c
@@ -1,25 +1,30 @@
 #include "../include/fractol.h"
 
-int	color_gradient(t_detail det, int iter)
+int	color_gradient(t_detail det, int iter, int max_iter, int start_color)
 {
-	det.max_iter = 1000;
-	det.grad_start = det.max_iter * 0.001;
-	det.grad_end = det.max_iter * 0.045;
-	det.start_color = 0xFF000080;
+	det.scaled_iter = (double)iter / max_iter;
+	det.t = pow(det.scaled_iter, 0.3);
 	det.end_color = 0xFFFFFFFF;
-	det.t = (iter - det.grad_start) / (det.grad_end - det.grad_start);
-	if (det.t < 0)
-		det.t = 0;
-	if (det.t > 1)
-		det.t = 1;
-	det.start_red = (det.start_color >> 16) & 0xFF;
-	det.start_green = (det.start_color >> 8) & 0xFF;
-	det.start_blue = det.start_color & 0xFF;
+	det.start_red = (start_color >> 16) & 0xFF;
+	det.start_green = (start_color >> 8) & 0xFF;
+	det.start_blue = start_color & 0xFF;
 	det.end_red = (det.end_color >> 16) & 0xFF;
 	det.end_green = (det.end_color >> 8) & 0xFF;
 	det.end_blue = det.end_color & 0xFF;
 	det.red = (int)((1 - det.t) * det.start_red + det.t * det.end_red);
 	det.green = (int)((1 - det.t) * det.start_green + det.t * det.end_green);
 	det.blue = (int)((1 - det.t) * det.start_blue + det.t * det.end_blue);
+	if (iter >= max_iter * 0.99)
+	{
+		det.red = det.red * (1 - det.scaled_iter) + 30 * det.scaled_iter;
+		det.green = det.green * (1 - det.scaled_iter) + 30 * det.scaled_iter;
+		det.blue = det.blue * (1 - det.scaled_iter) + 30 * det.scaled_iter;
+		if (iter == max_iter)
+		{
+			det.red = 15;
+			det.green = 15;
+			det.blue = 15;
+		}
+	}
 	return ((det.red << 16) | (det.green << 8) | det.blue);
 }
